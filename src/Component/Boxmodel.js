8465b1b80fee47afdab63ac9f40034bcf04b43f9
reetika <reetika.singh@byjus.com>
2021-04-24 09:53:45 +0530

    matrix element change condition added

--- src/Component/Boxmodel.js
+++ src/Component/Boxmodel.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export const Boxmodel = (props) =>{
+ const Boxmodel = (props) =>{
     return (
         <button onClick={props.onClick}>
             {props.children}
         </button>
     )
 }
+export default Boxmodel
--- src/Container/MainComponent.js
+++ src/Container/MainComponent.js
@@ -4,37 +4,62 @@ class MainComponent extends Component {
     constructor(props){
         super(props);
         this.state={
-            arr=[],
-
+            ans:[[-1, -1, -1],[-1,-1,-1], [-1,-1,-1]],
+            player1Enable: true,
+            player2Enable: false
         }
     }
 
-    changeState = ()=>{
+    checkCurrentStatus = () =>{
+        let { ans} = this.state
+        
+    }
+    handleBlockClick = (i, j) => {
+        let {player1Enable, player2Enable, ans} = this.state
+        if(ans[i][j] === -1){
+            let temp = player1Enable ? 1 : 0
+            ans[i][j]=temp
+            this.setState({
+                player1Enable: !player1Enable,
+                player2Enable: !player2Enable,
+                ans
+            })
+            this.checkCurrentStatus(i,j)
+        }
+    
+    }
 
+    changeState = ()=>{
+        let arr=[]
+        let {ans} = this.state
+        for(let i=0;i<3;i++)
+        {
+            let arr1=[]
+            for(let j=0;j<3;j++)
+            {
+                arr1.push(
+                    
+                    <Boxmodel onClick={this.handleBlockClick.bind(this, i, j)}>{ans[i][j]}</Boxmodel>
+               )
+            }
+
+            arr.push(
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    {arr1}
+                </div>
+            )
+        }
+        return arr;
+        // console.log("array",arr)
     }
     render() {
+      
         return (
             <div>
-                <div style={{display:'flex',flexDirection:'row'}}>
-                    <Boxmodel onClick={this.changeState} >0</Boxmodel>
-                    <Boxmodel  >0</Boxmodel>
-                    <Boxmodel  >0</Boxmodel>
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'row' }}>
-                    <Boxmodel  >0</Boxmodel>
-                    <Boxmodel  >0</Boxmodel>
-                    <Boxmodel  >0</Boxmodel>
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'row' }}>
-                    <Boxmodel  >0</Boxmodel>
-                    <Boxmodel  >0</Boxmodel>
-                    <Boxmodel  >0</Boxmodel>
-                </div>
-                
+                {this.changeState()}
             </div>
         )
     }
 }
 
 export default MainComponent
-//
